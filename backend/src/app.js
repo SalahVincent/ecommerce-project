3b05cd374e219c6e21bf15fcc6158aef746515ab
SalahVincent <vincentsalah3@gmail.com>
2026-02-20 12:25:45 +0100

    mount(fix): productRouter to app.js for endpoints to be reachable

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,35 +1,38 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import sequelize from './config/db.js';
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import sequelize from './config/db.js'
 import './models/product.js'
+import { errorHandler } from './middleware/errorMiddleware.js'
+import productRoutes from './routes/ProductRoutes.js'
 
 dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 app.use('/api/products', productRoutes)
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 async function startServer() {
   try {
-    await sequelize.authenticate();
-    console.log('🐘 Connection to PostgreSQL has been established successfully.');
+    await sequelize.authenticate()
+    console.log('🐘 Connection to PostgreSQL has been established successfully.')
 
-    await sequelize.sync({ alter: true });
-    console.log('📦 Database models synced.');
+    await sequelize.sync({ alter: true })
+    console.log('📦 Database models synced.')
 
     app.listen(PORT, () => {
-      console.log(`🚀 Server is running on http://localhost:${PORT}`);
-    });
+      console.log(`🚀 Server is running on http://localhost:${PORT}`)
+    })
   } catch (error) {
-    console.error('❌ Unable to connect to the database:', error);
-    process.exit(1);
+    console.error('❌ Unable to connect to the database:', error)
+    process.exit(1)
   }
 }
 
-startServer();
+startServer()
--- backend/src/routes/ProductRoutes.js
+++ backend/src/routes/ProductRoutes.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import { productController } from '../controllers/ProductController.js';
+import express from 'express'
+import { productController } from '../controllers/ProductController.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', productController.getAllProducts);
-router.get('/:id', productController.getOneProduct);
-router.post('/', productController.createProduct);
-router.put('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+router.get('/', productController.getAllProducts)
+router.get('/:id', productController.getOneProduct)
+router.post('/', productController.createProduct)
+router.put('/:id', productController.updateProduct)
+router.delete('/:id', productController.deleteProduct)
 
-export default router;
+export default router
